d425754a770ed73208023070122cd105008100a8
GitHubmedcharfi <mohamedcharfi4070@gmail.com>
2025-07-26 17:01:35 +0100

    Initial commit: Nginx log analyzer

---
+++ analyzer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LOG_FILE="access.log"
+
+if [[ ! -f "$LOG_FILE" ]]; then
+  echo "Log file not found: $LOG_FILE"
+  exit 1
+fi
+
+echo
+echo "Top 5 IP addresses with the most requests:"
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+
+echo
+echo "Top 5 most requested paths:"
+awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+
+echo
+echo "Top 5 response status codes:"
+awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+
+echo
+echo "Top 5 user agents:"
+awk -F\" '{print $6}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -5
+
